a0606fb376d1d09c99e26fbcb5c283d2b5e01b8a
benjaminhuanghuang <bhuang@rms.com>
2022-06-16 18:15:37 -0700

    bootsect

--- 01-bootsector/bootsect.s
+++ 01-bootsector/bootsect.s
@@ -7,11 +7,32 @@
 # 16-real mode address = SEG<<4 + OFFEST
 .equ BOOTSEG, $0x07c0
 
+.text
+
 ljump $BOOTSEG, $_bootstart
 
 _bootstart:
-  jmp _bootstart
+#Get cursor position
+  mov $0x03, %AH  
+  int $0x10
+    
+ # put address of string to ES:BP   
+  mov $BOOTSEG, %AX
+  mov %AX. %ES
+  mov $_string, %BP
+  mov $0x1301, %AX
+  mov $0x0007, %BX   # white
+  mov $12, %CX        # char count
+  int 0x10
+   
 
+loop:
+  jmp loop
+
+
+_string:
+  .ascii "Hello..."
+  .byte 13, 10, 13, 10    # new line
 
 # fill 0s to address 510
 .= 510
