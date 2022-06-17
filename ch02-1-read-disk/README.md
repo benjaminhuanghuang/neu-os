c6223233ce4edfe84093d4691ca6b952015254c2
benjamin huang <benjaminhuanghuang@gmail.com>
2022-06-17 09:32:14 -0700

    add notes

--- ch02-1-read-disk/README.md
+++ ch02-1-read-disk/README.md
@@ -2,3 +2,20 @@
 
 https://www.bilibili.com/video/BV1qx411p7yQ
 
+1. Create the boot disk 
+flappy的第一个扇区(512 Bytes)是bootsect， 第二个扇区是kerne sl
+
+```
+  dd if=bootsect of=Image bs=512 count=1
+	
+  dd if=kernel of=Image bs=512 count=4 seek=1   
+  # seek=1: 避免覆盖第一个section
+```
+
+BIOS在启动时把`bootsect`加载到内存`0x7C00`处并执行, `bootsect` 会把kernal加载到内存`0x10200`，并jump 到`0x10200`开始执行
+
+
+check bootsect size
+```
+  du -b bootsect
+```
--- ch02-1-read-disk/kernel.s
+++ ch02-1-read-disk/kernel.s
@@ -10,12 +10,19 @@ show_text:
   # put address of string to ES:BP   
   mov $DEMOSEG, %AX
   mov %AX, %ES
-  mov $msg, %BP
-  mov $0x1301, %ax
-  mov $0x0007, %bx   # white 
-  mov $LEN, %CX      # char count
+  mov $0x03, %AH
+  mov %BH, %ES
   int $0x10
 
+  mov $msg, %BP
+  mov $0x1301, %ax
+  mov $0x000a, %bx   # blue 
+  mov $LEN, %CX      # char count
+
+  int $0x10
+
+loop_forever:
+  jmp loop_forever
 msg:
   .byte 13, 10
   .ascii "kernel is running."
